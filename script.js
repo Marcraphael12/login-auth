8bb284a697f8696d315462b6937cfe117aac892f
Marc Raphael <marcraphael979@gmail.com>
2021-10-07 14:25:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+
+    document.getElementById('user_div').style.display = 'block'
+    document.getElementById('login_div').style.display = 'none'
+  } else {
+    // No user is signed in.
+
+    document.getElementById('user_div').style.display = 'none'
+    document.getElementById('login_div').style.display = 'block'
+  }
+})
+
+function login() {
+  var userEmail = document.getElementById('email_field').value
+  var userPass = document.getElementById('password_field').value
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(userEmail, userPass)
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code
+      var errorMessage = error.message
+
+      window.alert('Error : ' + errorMessage)
+
+      // ...
+    })
+}
+
+function logout() {
+  firebase.auth().signOut()
+}
