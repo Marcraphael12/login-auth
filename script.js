7c46fe1bc952683cae3cace0f7919688837dfc5a
Marc Raphael <marcraphael979@gmail.com>
2021-10-07 14:49:54 +0000

    Update main div style

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     // User is signed in.
 
-    document.getElementById('user_div').style.display = 'block'
+    document.getElementById('user_div').style.display = 'flex'
     document.getElementById('login_div').style.display = 'none'
   } else {
     // No user is signed in.
 
     document.getElementById('user_div').style.display = 'none'
-    document.getElementById('login_div').style.display = 'block'
+    document.getElementById('login_div').style.display = 'flex'
   }
 })
 
